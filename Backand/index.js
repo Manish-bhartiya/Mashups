37509daa76332707a3c7e84ddff6ac01457f6639
Manish-bhartiya <142502814+Manish-bhartiya@users.noreply.github.com>
2024-09-13 13:54:33 +0530

    main

--- Backand/index.js
+++ Backand/index.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  console.log("i am hit");
   next(); // Proceed to the next middleware or route handler
 });
 
@@ -26,6 +27,7 @@ app.options('*', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  console.log("i am hit");
   res.sendStatus(204); // Respond with no content for OPTIONS requests
 });
 
