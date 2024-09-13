7863c7c75278706ae02daf2df95f5767a7f49943
Manish-bhartiya <142502814+Manish-bhartiya@users.noreply.github.com>
2024-09-13 12:27:02 +0530

    main

--- Backand/index.js
+++ Backand/index.js
@@ -17,7 +17,7 @@ const app = express();
 // Configure CORS to allow requests from your frontend domain
 const corsOptions =  {
   
-    origin: "http://localhost:4001",
+    origin: "https://mashups-dbea.vercel.app/",
     credentials:true,
     methods: ["GET", "POST"]
   
--- Mashups/vite.config.js
+++ Mashups/vite.config.js
@@ -3,20 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    port:'5173',
-    strictPort:true,
-    headers:{
-      a:'b',
-      // 'access-control-allow-origin':'*',
-    },
-    proxy:{
-      '/api':{
-        target:'https://mashupsbackand.vercel.app/',
-        changeOrigin:true,
-        rewrite:(path) => path.replace(/^\/api/,'')
-      }
-    }
-  },
-  plugins: [react()],
+  // server:{
+  //   port:'5173',
+  //   strictPort:true,
+  //   headers:{
+  //     a:'b',
+  //     // 'access-control-allow-origin':'*',
+  //   },
+  //   proxy:{
+  //     // '/api':{
+  //     //   target:'https://mashupsbackand.vercel.app/',
+  //     //   changeOrigin:true,
+  //     //   rewrite:(path) => path.replace(/^\/api/,'')
+  //     // }
+  //   }
+  // },
+  // plugins: [react()],
 })
