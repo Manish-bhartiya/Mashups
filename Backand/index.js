68bab33c0a1acda70e71837d48d93da22113530d
Manish-bhartiya <142502814+Manish-bhartiya@users.noreply.github.com>
2024-09-13 09:52:37 +0530

    main

--- Backand/index.js
+++ Backand/index.js
@@ -24,10 +24,7 @@ const app = express();
 // });
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: "https://mashupsbackand.vercel.app/",
-  credentials: true
-})); // Apply CORS with specified options
+app.use(cors); // Apply CORS with specified options
 
 // Initialize database connection
 db();
