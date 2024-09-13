03ee8be45321d44d4f2330b9cc2f7a6e40c18391
Manish-bhartiya <142502814+Manish-bhartiya@users.noreply.github.com>
2024-09-13 13:51:08 +0530

    main

--- Backand/index.js
+++ Backand/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Custom middleware to set CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mashups-dbea.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next(); // Proceed to the next middleware or route handler
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 // Handle preflight requests
 app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mashups-dbea.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.sendStatus(204); // Respond with no content for OPTIONS requests
