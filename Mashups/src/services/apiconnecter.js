fc76443d36b2658f1356dbbd8e94a8a53abbc74c
Manish-bhartiya <bhartiyamanish845@gmail.com>
2024-09-28 08:15:21 +0530

    Mashups

--- Mashups/src/services/apiconnecter.js
+++ Mashups/src/services/apiconnecter.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const axiosinstance = axios.create({});
 
-// const Base_url = "https://mashupsbackand.vercel.app/api";
-const Base_url = "http://localhost:4001/api/";
+const Base_url = "https://mashupsbackand.vercel.app/api/";
+// const Base_url = "https://mashupsbackand.vercel.app/"
+// const Base_url = "http://localhost:4001/api/";
 
 export const apiconnecter = (method,url,bodydata,headers,params) => {
     return axiosinstance({
